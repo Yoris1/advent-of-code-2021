5688273fc51de62194a143b8a47afefea6150d05
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-12-03 16:14:42 +0200

    nasty but works

--- day3/part_2.c
+++ day3/part_2.c
@@ -2,10 +2,10 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
-#define BUFF_COUNT 1000
+#define BUFF_COUNT 1001
 #define BUFF_LENGTH 24
 
-int get_most_common_bit(char buffer[BUFF_COUNT][BUFF_LENGTH], int entry_count, int digit, bool ignore[BUFF_COUNT]) {
+int get_most_common_bit(char buffer[BUFF_COUNT][BUFF_LENGTH], int entry_count, int digit, bool ignore[BUFF_COUNT], bool oxygen) {
 	int zeroes = 0;
 	int ones = 0;
 	for(int i = 0; i < entry_count; i++) {
@@ -14,20 +14,53 @@ int get_most_common_bit(char buffer[BUFF_COUNT][BUFF_LENGTH], int entry_count, i
 		}
 		if(buffer[i][digit] == '1')
 			ones ++;
-		else 
+		else if(buffer[i][digit] == '0')
 			zeroes++;
 	}
-	return ones>zeroes?1:0;
+	if(oxygen) {
+		if(ones == zeroes) return 1;
+		return ones>zeroes?1:0;
+	}
+	else {
+		if(ones == zeroes) return 0;
+		return ones>zeroes?0:1;
+	}
 }
-
-int get_rating(char buffer[BUFF_COUNT][BUFF_LENGTH], int entry_count, int digit_length) {
+int binary_string_to_int(char str[BUFF_LENGTH], int digits) {
+	int result = 0;
+	for(int i = 0; i< digits; i++) {
+		if(str[i] == '1') {
+			result += 1<<(digits-i-1);
+		}
+	}
+	return result;
+}
+int get_rating(char buffer[BUFF_COUNT][BUFF_LENGTH], int entry_count, int digit_length, bool oxygen) {
 	bool ignore[BUFF_COUNT];
 	for(int i = 0; i < BUFF_COUNT; i++) 
 		ignore[i] = false;
 	
 	for(int i = 0; i<digit_length; i++) {
-		int common = get_most_common_bit(buffer, entry_count, i, ignore);
-		printf("Common: %d\n", common);
+		int common = get_most_common_bit(buffer, entry_count, i, ignore, oxygen);
+		int kept = 0;
+		for(int j = 0; j < entry_count; j++) {
+			if(buffer[j][i] == common+0x30 && !ignore[j]) {
+				//printf("Digit: %d(%d) Keeping: %s",i, common,  buffer[j]);
+				kept++;
+			} 
+			else {
+				ignore[j] = true;
+			}
+		}
+		if(kept == 1) {
+			for(int j = 0; j < entry_count; j++) {
+				if(!ignore[j]) {
+
+					//printf("Result: %s", buffer[j]);
+					return binary_string_to_int(buffer[j], digit_length);
+				}
+			}
+		}
 	}
 	return 0;
 }
@@ -35,20 +68,16 @@ int get_rating(char buffer[BUFF_COUNT][BUFF_LENGTH], int entry_count, int digit_
 int main() {
 	FILE *input = fopen("input.inputfile", "r");
 
-	int ones[24];
-	int zeroes[24];
-	for(int i = 0; i< 24; i++) {
-		ones[i] = 0;
-		zeroes[i] = 0;
-	}
-
-
-	char buffer[1000][24];
+	char buffer[BUFF_COUNT][BUFF_LENGTH];
 	int i = 0;
+	buffer[0][1] = 'T';
 	while (true)
-		if(fgets(buffer[i++], 23, input) == NULL) break; // reserving the first bit for ability to discard things
+		if(fgets(buffer[i++], 24, input) == NULL) break; // reserving the first bit for ability to discard things
 	fclose(input);
 
 	int digit_length = strlen(buffer[0])-1;
-	get_rating(buffer, i, digit_length);
+
+	int o2=get_rating(buffer, i, digit_length, true);
+	int co2 = get_rating(buffer, i, digit_length, false);
+	printf("CO2: %d, O2: %d, life support: %d\n", co2, o2, co2*o2);
 }
