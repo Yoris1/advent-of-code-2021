980192daf5ea7ff6a9c4e65f738860ea53d0dd82
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-12-07 03:37:09 +0200

    clean up code

--- day6/part1.c
+++ day6/part1.c
@@ -3,7 +3,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <inttypes.h>
-#include <string.h>
+
+#define SIMULATION_DAYS 256
 
 void print_fishies(uint64_t fishies[9]) {
 	for(int i = 0; i < 9; i++)
@@ -12,29 +13,26 @@ void print_fishies(uint64_t fishies[9]) {
 
 void add_day(uint64_t fishies[9]) {
 	uint64_t new_fish = fishies[0];
-	for(int i = 0; i < 6; i++) {
+	for(int i = 0; i < 6; i++)
 		fishies[i] = fishies[i+1];
-	}
+	
 	fishies[6] = fishies[7]+new_fish;
 	fishies[7] = fishies[8];
 	fishies[8] = new_fish;
 }
-uint64_t sum_fishes(uint64_t fishies[9]) {
+
+uint64_t count_fish(uint64_t fishies[9]) {
 	uint64_t sum = 0;
-	for(int i = 0; i < 9; i++) {
+	for(int i = 0; i < 9; i++)
 		sum += fishies[i];
-	}
 	return sum;
 }
 
 int main() {
+	uint64_t fishies[9] = {0};
 
-	uint64_t fishies[9];
-	memset(fishies, 0, sizeof(uint64_t)*9);
+	FILE* input_file = fopen("input.inputfile", "r");
 
-	FILE* input_file = fopen("input.inputfile", "r");	
-	
-	size_t i = 0;
 	char in = 0;
 	while(in = fgetc(input_file)) {
 		if(feof(input_file))
@@ -43,16 +41,16 @@ int main() {
 			continue;
 		fishies[in-0x30] += 1;
 	}
-
 	fclose(input_file);
 
-	for(int i = 0; i<256; i++)
+	for(int i = 0; i<SIMULATION_DAYS; i++)
 		add_day(fishies);
-	uint64_t total_fishes=sum_fishes(fishies);
-	if(total_fishes>INT64_MAX) {
+	uint64_t total_fish=count_fish(fishies);
+
+	if(total_fish>INT64_MAX)
 		printf("overflow!\n");
-	}
-	printf("there are %" PRIu64 " fishes after 256 days\n", total_fishes);
+	
+	printf("there will be a total of %" PRIu64 " fish after %i days\n", total_fish, SIMULATION_DAYS);
 	return 0;
 
 }
