48f11f4fb9f099e6305bcb12d5d2a96fdc306881
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-12-07 03:03:46 +0200

    new approach works on example data

--- day6/part1.c
+++ day6/part1.c
@@ -5,36 +5,40 @@
 #include <string.h>
 
 #define INPUT_CHAR_ARRAY 50
-void add_day(int8_t* fishies, size_t* fish_count) {
-	size_t new_fish_count = 0;
-	for(size_t x = 0; x < *fish_count; x++) {
-		if(fishies[x] == 0) new_fish_count++;
-	}
-	fishies = realloc(fishies, *fish_count+new_fish_count);
 
-	int new_fish_added = 0;
+void print_fishies(int fishies[9]) {
 
-	for(size_t i = 0; i < *fish_count; i++) {
-		int8_t fish = fishies[i];
-		if(fish == 0) {
-			fishies[i] = 7;
-			fishies[*fish_count+(new_fish_added++)] = 8;
-		}
-		fishies[i] --;
+	for(int i = 0; i < 9; i++) {
+		printf("There are %i fishies whose internal timer is %i days\n", fishies[i], i);
 	}
-	*fish_count = *fish_count+new_fish_count;
 }
-void print_fishes(int8_t* fishies, size_t* fish_count) {
-	for(size_t i = 0; i < *fish_count; i++) {
-		printf("fish: %i\n", fishies[i]);
-	}
 
+void add_day(int fishies[9]) {
+	int new_fish = fishies[0];
+	// todo replace all with 128bit + ints;
+	fishies[0] = fishies[1];
+	fishies[1] = fishies[2];
+	fishies[2] = fishies[3];
+	fishies[3] = fishies[4];
+	fishies[4] = fishies[5];
+	fishies[5] = fishies[6];
+	fishies[6] = fishies[7]+new_fish;
+	fishies[7] = fishies[8];
+	fishies[8] = new_fish;
+}
+int sum_fishes(int fishies[9]) {
+	int sum = 0;
+	for(int i = 0; i < 9; i++) {
+		sum += fishies[i];
+	}
+	return sum;
 }
 
 int main() {
+
+	int fishies[9];
+	memset(fishies, 0, sizeof(int)*9);
 	FILE* input_file = fopen("input.inputfile", "r");	
-	char converted_buffer[INPUT_CHAR_ARRAY];
-	memset(converted_buffer, 15, INPUT_CHAR_ARRAY);
 	
 	size_t i = 0;
 	char in = 0;
@@ -45,28 +49,15 @@ int main() {
 			continue;
 		in -= 0x30;
 		printf("Read digit %i\n", in);
-		converted_buffer[i++] = in;
+		fishies[in] += 1;
 	}
 
 	fclose(input_file);
 
-	printf("starting fish count: %i\n", i);
+	for(int i = 0; i<80; i++)
+		add_day(fishies);
 
-	int8_t* fishes = malloc(i*sizeof(int8_t));
-	int y = 0;
-	for(int x = 0 ; x < i; x++) {
-		fishes[x] = converted_buffer[x];
-	}
-	print_fishes(fishes, &i);
-	
-	for(int j = 0; j < 18; j++) {
-		add_day(fishes, &i);
-		printf("Fishies after %i day count: %i\n",j+1, i);
-		print_fishes(fishes, &i);
-	}
-
-
-	
+	printf("there are %i fishes after 80 days\n", sum_fishes(fishies));
 	return 0;
 
 }
