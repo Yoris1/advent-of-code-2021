198c0a10ceb772f7590e9841f03717c4f9114354
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-12-07 02:29:09 +0200

    remove int packaging. using this to model growth

--- day6/part1.c
+++ day6/part1.c
@@ -2,47 +2,41 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 
 #define INPUT_CHAR_ARRAY 50
-int8_t* add_day(int8_t* fishies, __int128_t* fish_count) {
-	int new_fish_count = 0;
-	for(int x = 0; x < fish_count; x++) {
-		int8_t fish1, fish2;
-		int_to_two_ints(fishies[x], &fish1, &fish2);
-			if(fish1 == 0) {
-				new_fish_count++;
-			}
-			if(fish2 == 0) {
-				new_fish_count++;
-			}
+void add_day(int8_t* fishies, size_t* fish_count) {
+	size_t new_fish_count = 0;
+	for(size_t x = 0; x < *fish_count; x++) {
+		if(fishies[x] == 0) new_fish_count++;
 	}
-	
-	return NULL;
-}
-void int_to_two_ints(int8_t in, int8_t* a, int8_t* b) {
-	*a = in&0xf;
-	*b = (in>>4)&0xf;
-}
-void print_fish_array(int8_t* fishies, const __int128_t* length) {
-	for(int i = 0 ;i < *length; i++) {
-		int8_t a,b;
-		int_to_two_ints(fishies[i], &a, &b);
-		printf("Fish %i\n",a);
-		printf("Fish %i\n",b);
+	fishies = realloc(fishies, *fish_count+new_fish_count);
+
+	int new_fish_added = 0;
+
+	for(size_t i = 0; i < *fish_count; i++) {
+		int8_t fish = fishies[i];
+		if(fish == 0) {
+			fishies[i] = 7;
+			fishies[*fish_count+(new_fish_added++)] = 8;
+		}
+		fishies[i] --;
 	}
+	*fish_count = *fish_count+new_fish_count;
+}
+void print_fishes(int8_t* fishies, size_t* fish_count) {
+	for(size_t i = 0; i < *fish_count; i++) {
+		printf("fish: %i\n", fishies[i]);
+	}
+
 }
 
-int8_t two_ints_to_one(int8_t a, int8_t b) {
-	int8_t res;
-	res = a;
-	res += b << 4;
-}
 int main() {
 	FILE* input_file = fopen("input.inputfile", "r");	
 	char converted_buffer[INPUT_CHAR_ARRAY];
 	memset(converted_buffer, 15, INPUT_CHAR_ARRAY);
 	
-	__int128_t i = 0;
+	size_t i = 0;
 	char in = 0;
 	while(in = fgetc(input_file)) {
 		if(feof(input_file))
@@ -53,23 +47,24 @@ int main() {
 		printf("Read digit %i\n", in);
 		converted_buffer[i++] = in;
 	}
+
 	fclose(input_file);
 
 	printf("starting fish count: %i\n", i);
-	i = (i+i%2)/2;
-	
-
 
 	int8_t* fishes = malloc(i*sizeof(int8_t));
 	int y = 0;
 	for(int x = 0 ; x < i; x++) {
-		int8_t fish1 = converted_buffer[y++];
-		int8_t fish2 = converted_buffer[y++];
-		fishes[x] = two_ints_to_one(fish1, fish2);
+		fishes[x] = converted_buffer[x];
+	}
+	print_fishes(fishes, &i);
+	
+	for(int j = 0; j < 18; j++) {
+		add_day(fishes, &i);
+		printf("Fishies after %i day count: %i\n",j+1, i);
+		print_fishes(fishes, &i);
 	}
 
-
-	print_fish_array(fishes, &i);
 
 	
 	return 0;
