e81e53bd8a0820766e9df4a84cc23d11b5ffd6d9
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-12-07 03:32:54 +0200

    make work for part 2

--- day6/part1.c
+++ day6/part1.c
@@ -2,32 +2,25 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <string.h>
 
-#define INPUT_CHAR_ARRAY 50
-
-void print_fishies(int fishies[9]) {
-
-	for(int i = 0; i < 9; i++) {
+void print_fishies(uint64_t fishies[9]) {
+	for(int i = 0; i < 9; i++)
 		printf("There are %i fishies whose internal timer is %i days\n", fishies[i], i);
-	}
 }
 
-void add_day(int fishies[9]) {
-	int new_fish = fishies[0];
-	// todo replace all with 128bit + ints;
-	fishies[0] = fishies[1];
-	fishies[1] = fishies[2];
-	fishies[2] = fishies[3];
-	fishies[3] = fishies[4];
-	fishies[4] = fishies[5];
-	fishies[5] = fishies[6];
+void add_day(uint64_t fishies[9]) {
+	uint64_t new_fish = fishies[0];
+	for(int i = 0; i < 6; i++) {
+		fishies[i] = fishies[i+1];
+	}
 	fishies[6] = fishies[7]+new_fish;
 	fishies[7] = fishies[8];
 	fishies[8] = new_fish;
 }
-int sum_fishes(int fishies[9]) {
-	int sum = 0;
+uint64_t sum_fishes(uint64_t fishies[9]) {
+	uint64_t sum = 0;
 	for(int i = 0; i < 9; i++) {
 		sum += fishies[i];
 	}
@@ -36,8 +29,9 @@ int sum_fishes(int fishies[9]) {
 
 int main() {
 
-	int fishies[9];
-	memset(fishies, 0, sizeof(int)*9);
+	uint64_t fishies[9];
+	memset(fishies, 0, sizeof(uint64_t)*9);
+
 	FILE* input_file = fopen("input.inputfile", "r");	
 	
 	size_t i = 0;
@@ -47,17 +41,18 @@ int main() {
 			break;
 		if(in == ',')
 			continue;
-		in -= 0x30;
-		printf("Read digit %i\n", in);
-		fishies[in] += 1;
+		fishies[in-0x30] += 1;
 	}
 
 	fclose(input_file);
 
-	for(int i = 0; i<80; i++)
+	for(int i = 0; i<256; i++)
 		add_day(fishies);
-
-	printf("there are %i fishes after 80 days\n", sum_fishes(fishies));
+	uint64_t total_fishes=sum_fishes(fishies);
+	if(total_fishes>INT64_MAX) {
+		printf("overflow!\n");
+	}
+	printf("there are %" PRIu64 " fishes after 256 days\n", total_fishes);
 	return 0;
 
 }
