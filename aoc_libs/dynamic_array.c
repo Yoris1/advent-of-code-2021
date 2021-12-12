f1d15f2c8beb6cf78e81c8751280a8b876d3d730
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-12-12 22:01:30 +0200

    create a dynamic array library cause i need it

---
+++ aoc_libs/dynamic_array.c
@@ -0,0 +1,34 @@
+#include "dynamic_array.h"
+#include <stdlib.h>
+
+d_array create_dynamic_array(size_t initial_length, size_t element_size) {
+	d_array arr;
+	initial_length = initial_length<1?1:initial_length;
+	
+	arr.raw = malloc(initial_length*element_size);
+	arr.count = 0;
+
+	arr._length = initial_length;
+	arr._element_size = element_size;
+	return arr;
+}
+
+void add_to_dynamic_array(d_array* arr, const void* item) {
+	if(arr->count >= arr->_length) {
+		arr->_length += 10;
+		arr->raw = realloc(arr->raw, arr->_element_size * arr->_length);
+	}
+	
+	char* raw = arr->raw;
+	for(size_t i = 0; i < arr->_element_size; i++) {
+		unsigned char byte = ((char*)item)[i];
+		raw[arr->count*arr->_element_size+i] = byte;
+	}
+	arr->count ++;
+}
+
+void delete_dynamic_array(d_array* arr) {
+	free(arr->raw);
+	arr->count = 0;
+	arr->_length = 0;
+}
